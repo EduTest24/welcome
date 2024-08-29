513eff9bfb9dfe0b7f02d159c7df378b27d7eae7
EduTest24 <edutest84@gmail.com>
2024-08-29 10:36:25 +0530

    Added files for Welcome Page.
    Welcome page which is a redirection page to the main Platform of ShareIO.

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hero").addEventListener("click", function () {
+  window.location.href =
+    "https://script.google.com/macros/s/AKfycbxhySferk8ObmJY3BwN3JMzF2SFc2J5yd-oOc5QhFCNnB1H2h8BsJaJwPZQVk4osC5k/exec"; // Replace with your target URL
+});
