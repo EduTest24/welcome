68b9e696def9e3edff78212051034a613e268f6a
EduTest24 <edutest84@gmail.com>
2024-08-29 17:46:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("hero").addEventListener("click", function () {
   window.location.href =
-    "https://script.google.com/macros/s/AKfycbxhySferk8ObmJY3BwN3JMzF2SFc2J5yd-oOc5QhFCNnB1H2h8BsJaJwPZQVk4osC5k/exec"; // Replace with your target URL
+    "https://script.google.com/macros/s/AKfycbzsDK8oM17cIP7kxwKdDacOwxTxRlgP2CeHWIzA0TDVF6pZYe_S11YfiwlEL8BsKOYI/exec"; // Replace with your target URL
 });
